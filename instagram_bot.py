6db2e186877a9c23923e38a742f8540a55d506eb
Zaytsev Dmitriy <zaytsev_dmitriy@edadeal.ru>
2020-12-22 19:55:38 +0300

    new private instagram api library

--- instagram_bot.py
+++ instagram_bot.py
@@ -8,14 +8,11 @@ import random
 import time
 
 import requests
+from instagram_private_api import Client
 from user_agent import generate_user_agent
 
 from constraint import (
     BASE_URL,
-    URL_LIKES,
-    URL_LOGIN,
-    URL_LOGOUT,
-    URL_MEDIA_DETAIL,
 )
 
 
@@ -25,38 +22,26 @@ class InstagramBot:
     """
 
     accept_language = "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4"
+    user_agent = (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+        "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 "
+        "Safari/605.1.15"
+    )
 
     def __init__(self, login, password, target, log_to_file=False):
 
         self.bot_start = datetime.datetime.now()
-
         self.user_login = login.lower()
+        self.password = password
         self.user_password = password
         self.target = target.split(",")
-        self.login_status = False
         self.login_post = None
+        self.user_login = login
+        self.password = password
         self.csrftoken = None
+        self.client = None
         self.log_to_file = log_to_file
         self.session = requests.Session()
-
-        now_time = datetime.datetime.now().strftime("%d.%m.%Y %H:%M")
-        log_string = f"InstagramBot started at {now_time}:\n"
-
-        if self.log_to_file:
-            formatter = logging.Formatter(
-                "%(asctime)s - %(name)s - %(message)s",
-            )
-            self.logger = logging.getLogger(self.user_login)
-            hdrl = logging.FileHandler(f"{self.user_login}.log", mode="w")
-            hdrl.setFormatter(formatter)
-            self.logger.setLevel(level=logging.INFO)
-            self.logger.addHandler(hdrl)
-        self.write_log(log_string)
-
-    def login(self):
-        login_time = int(datetime.datetime.now().timestamp())
-        log_string = "Trying to login as %s...\n" % self.user_login
-        self.write_log(log_string)
         self.session.cookies.update(
             {
                 "sessionid": "",
@@ -68,12 +53,6 @@ class InstagramBot:
                 "ds_user_id": "",
             }
         )
-        self.login_post = {
-            "username": self.user_login,
-            "enc_password": f"#PWD_INSTAGRAM_BROWSER:0:{login_time}:{self.user_password}",  # noqa
-            "queryParams": {},
-            "optIntoOneTap": "false",
-        }
         self.session.headers.update(
             {
                 "Accept-Encoding": "gzip, deflate",
@@ -89,48 +68,20 @@ class InstagramBot:
             }
         )
 
-        r = self.session.get(BASE_URL)
-        self.session.headers.update({"X-CSRFToken": r.cookies["csrftoken"]})
+        if self.log_to_file:
+            formatter = logging.Formatter(
+                "%(asctime)s - %(name)s - %(message)s",
+            )
+            self.logger = logging.getLogger(self.user_login)
+            hdrl = logging.FileHandler(f"{self.user_login}.log", mode="w")
+            hdrl.setFormatter(formatter)
+            self.logger.setLevel(level=logging.INFO)
+            self.logger.addHandler(hdrl)
 
-        time.sleep(5 * random.random())
-        login = self.session.post(URL_LOGIN, data=self.login_post)
-        self.session.headers.update(
-            {"X-CSRFToken": login.cookies["csrftoken"]}
-        )
-        self.csrftoken = login.cookies["csrftoken"]
-        time.sleep(5 * random.random())
-
-        if login.status_code == 200:
-            r = self.session.get("https://www.instagram.com/")
-            finder = r.text.find(self.user_login)
-            if finder != -1:
-                self.login_status = True
-                log_string = f"{self.user_login} login success!"
-                self.write_log(log_string)
-                return True
-            else:
-                self.login_status = False
-                self.write_log("Login error! Check your login data!")
-                return False
-        else:
-            self.write_log("Login error! Connection error!")
-            return False
-
-    def logout(self):
-        work_time = datetime.datetime.now() - self.bot_start
-        log_string = "Bot work time: %s" % work_time
-        self.write_log(log_string)
-
-        try:
-            logout_post = {"csrfmiddlewaretoken": self.csrftoken}
-            self.session.post(URL_LOGOUT, data=logout_post)
-            self.write_log("Logout success!")
-            self.login_status = False
-        except Exception as e:
-            self.write_log("Logout error! %s" % e)
+    def login(self):
+        self.client = Client(self.user_login, self.password)
 
     def writer_file(self, target_file, profile_data, already_liked_nodes):
-
         with open(target_file, "a") as f:
             for media in profile_data["graphql"]["user"][
                 "edge_owner_to_timeline_media"
@@ -138,35 +89,24 @@ class InstagramBot:
                 "edges"
             ]:  # noqa
                 if media["node"]["id"] not in already_liked_nodes:
-                    if not self.login_status:
-                        logged = self.login()
-                        if not logged:
-                            return
-
-                    media = self.session.get(
-                        URL_MEDIA_DETAIL % media["node"]["shortcode"]
-                    )
-                    if media.status_code != 200:
-                        self.write_log(
-                            "Media request returned %d status code"
-                            % media.status_code,
-                        )
-                        continue
-                    media_data = json.loads(media.text)
-                    media_info = media_data["graphql"]["shortcode_media"]
-                    if not media_info["viewer_has_liked"]:
-                        self.like(media_info["id"])
+                    if not self.client:
+                        self.login()
+                    data = self.client.media_info(media["node"]["id"])
+                    if not data["items"][0]["has_liked"]:
+                        self.client.post_like(data["items"][0]["id"])
                         sleep_time = random.randint(5, 15)
                         self.write_log("Sleeping %d" % sleep_time)
                         time.sleep(sleep_time)
-                    f.write(media_info["id"] + "\n")
+                    f.write(media["node"]["id"] + "\n")
 
     def like_all_users_media(self):
         """ Send http request to like target's feed """
-
         for target in self.target:
             try:
-                resp = self.session.get(os.path.join(BASE_URL, target))
+                resp = self.session.get(
+                    os.path.join(BASE_URL, target),
+                    headers={"User-agent": self.user_agent},
+                )
                 try:
                     data = json.loads(
                         resp.text.split("window._sharedData = ")[1].split(
@@ -198,32 +138,7 @@ class InstagramBot:
 
             except Exception as e:
                 self.write_log("An error occurred %s" % e)
-            if self.login_status:
-                self.logout()
         return
-
-    def like(self, media_id):
-        """ Send http request to like media by ID """
-        if self.login_status:
-            url_likes = URL_LIKES % media_id
-            try:
-                res = self.session.post(url_likes)
-                if res.status_code != 200:
-                    if res.status_code == 400:
-                        # in case you get banned sleep 5 minutes
-                        time.sleep(60 * 5)
-                        res = self.session.post(url_likes)
-                        if res.status_code != 200:
-                            self.write_log(
-                                "Except on like! Status code %d"
-                                % res.status_code,
-                            )
-                            return
-                self.write_log("Successfully liked %s" % media_id)
-
-            except Exception as e:
-                self.write_log("Except on like! %s" % e)
-            return
 
     def write_log(self, log_text):
         if self.log_to_file:
